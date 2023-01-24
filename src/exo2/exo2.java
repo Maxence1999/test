e430f8394e637a43c0db72eaa908942aaada8565
59013-42-12 <59013-42-12@exchange.ad.afpanet>
2023-01-24 15:51:15 +0100

    first commit

---
+++ src/exo2/exo2.java
@@ -0,0 +1,24 @@
+package exo2;
+
+import java.util.Scanner; 
+
+public class exo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner (System.in) ;
+		
+		int a;
+		
+		System.out.print( " veuillez saisir un nombre " );
+		
+		a = s.nextInt();
+		
+		a = a*a;
+		
+		System.out.print(a);
+		
+	}
+
+}
